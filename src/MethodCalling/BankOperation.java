a4f25f38a29595c9564961366ab0aabde958c21e
NeeruParmar <neeruparmarca@gmail.com>
2022-01-29 19:51:52 -0500

    Ques:DepositMoneyCode

--- src/MethodCalling/BankOperation.java
+++ src/MethodCalling/BankOperation.java
@@ -2,30 +2,28 @@ package MethodCalling;
 
 public class BankOperation {
 //Create two classes Main class and Bank operation class.
-	//Create two methods, one to deposit money into the account and another one which will verify the card no and pin. 
-	//Call the cardVerification method in depositMoney method before depositing the money
-	
-	
-	
-	String cardNumber= "5222222222222222";
+	// Create two methods, one to deposit money into the account and another one
+	// which will verify the card no and pin.
+	// Call the cardVerification method in depositMoney method before depositing the
+	// money
+
+	String cardNumber = "5222222222222222";
 	int pinNumber = 1100;
 	double newBalance = 0.0;
 	double totalAccountBal = 150.0;
-	
-	
-	boolean cardVerification (String cardNum, int pinCode) {
-	if (cardNum.equals (cardNumber) && (pinCode== pinNumber));
-	return true;
+
+	boolean cardVerification(String cardNum, int pinCode) {
+		if (cardNum.equals(cardNumber) && (pinCode == pinNumber))
+			;
+		return true;
 	}
-	
-	
-	
-	double depsoitMoney (double amountTobedeposit, String cardNum, int pinCode) {
-		System.out.println("Welcome to Your Acc: Select Amount to be deposited "+  cardVerification(cardNum, pinCode));
-		System.out.println("Amount Selected "+amountTobedeposit);
-		if (amountTobedeposit>0 ){
-			newBalance=totalAccountBal+amountTobedeposit;
-			
+
+	double depsoitMoney(double amountTobedeposit, String cardNum, int pinCode) {
+		System.out.println("Welcome to Your Acc: Select Amount to be deposited " + cardVerification(cardNum, pinCode));
+		System.out.println("Amount Selected " + amountTobedeposit);
+		if (amountTobedeposit > 0) {
+			newBalance = totalAccountBal + amountTobedeposit;
+
 		}
 		return newBalance;
 	}
--- src/MethodCalling/MainClass.java
+++ src/MethodCalling/MainClass.java
@@ -5,16 +5,15 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		BankOperation bankOperation = new BankOperation();
-		double newtotalAccountBal= 10;
-		double amountTobedeposit= 10;
-		
+		double newtotalAccountBal = 10;
+		double amountTobedeposit = 10;
 
 		double depositMoney = bankOperation.depsoitMoney(10, "5222222222222222", 1100);
-		if (depositMoney >0 ) {
-			System.out.println("New Balance" + (newtotalAccountBal+ amountTobedeposit));
+		if (depositMoney > 0) {
+			System.out.println("New Balance" + (newtotalAccountBal + amountTobedeposit));
 		} else {
 			System.out.println("No deposit done:");
-			}
+		}
 
 	}
 
