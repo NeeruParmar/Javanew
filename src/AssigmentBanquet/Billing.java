f6a56a95bd7192e663bc2a142a5ebca8c61b3661
NeeruParmar <neeruparmarca@gmail.com>
2022-02-24 20:13:34 -0500

    Corrected Code

--- src/AssigmentBanquet/Billing.java
+++ src/AssigmentBanquet/Billing.java
@@ -9,22 +9,56 @@ public class Billing {
 
 		Scanner scan = new Scanner(System.in);
 
-		Banquet banquet = new Banquet();
+		Banquet banquet = new Banquet(0.20, 0.10, 0.10);
 
-		System.out.println("Please Enter Cost of Food");
-		int costOfFood = scan.nextInt();
+		System.out.println("Welcome to  Blossom Banquet");
+		System.out.println("these are following information before booking ");
+		System.out.println("1. Base booking cost cannot be less than $100");
+		System.out.println(
+				"2. cost of food, beverage cannot be $0,0 but tip is optional, you can add tip vased on experience");
+		System.out.println(
+				"3. Banquet has maximum capacity of 500 guest  with minimum number of guest should be more than 30");
+		System.out.println("4. Food and beverage  tax will be applicable in addition to the total tax");
 
-		System.out.println("Please Enter Cost of Beverages");
-		int costOfBeverages = scan.nextInt();
-		System.out.println(" Enter Amount of Tip");
-		int tipCharge = scan.nextInt();
-		int BaseCost = banquet.calculateBaseCost(costOfFood, costOfBeverages, tipCharge);
+		System.out.println("ENTER THE BASE COST: $ ");
 
-		System.out.println("Enter the number of Guest");
-		int numOfGuest = scan.nextInt();
+		banquet.setbaseCost(scan.nextDouble());
+		if (banquet.getBaseCost() >= 100) {
+			System.out.println("ENTER THE FOOD COST: $");
+			banquet.setCostOfFood(scan.nextDouble());
 
-		double totalCost = banquet.calculateTheTotalCost(BaseCost, numOfGuest);
-		System.out.println("Total bill Cost  " + totalCost);
+			if (banquet.getCostOfFood() > 0.0) {
+				System.out.println("Enter BEVERAGE COST: $");
+				banquet.setCostOfBeverages(scan.nextDouble());
 
+				if (banquet.getCostOfBeverages() > 0.0) {
+					System.out.println("Enter NUMBER OF GUEST: ");
+					int numberOfGuest = scan.nextInt();
+					if (numberOfGuest >= 30 && numberOfGuest <= 500) {
+						System.out.println("Enter tip amt (optional): $");
+						banquet.tipCharge = scan.nextDouble();
+
+						double totalBaseCost = banquet.calculateBaseCost(banquet.getBaseCost(), banquet.getCostOfFood(),
+								banquet.getCostOfBeverages());
+
+						double finalAmtToBePaid = banquet.calculateTheTotalCost(totalBaseCost,
+								banquet.getTotalTax(totalBaseCost), numberOfGuest,
+								banquet.getCessTax(numberOfGuest, totalBaseCost));
+
+						System.out.println("Billing Amt" + finalAmtToBePaid);
+					} else {
+						System.out.println("invalid number oF guest entered please enter correct number ");
+					}
+
+				} else {
+					System.out.println("Invalid amt entered for beverage Cost, please follow instruction ");
+				}
+
+			} else {
+				System.out.println("invalid amt entered for food Cost, please enter correct value");
+			}
+		} else {
+			System.out.println("Invalid amount entered , base booking cost need to more than $100");
+		}
 	}
 }
